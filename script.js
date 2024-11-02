eb9700b77f1f77c2e30ce3ac8cb04c5f3d1800ad
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-02 12:25:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	function insert_Row() {
+    //Write your code here
+	
+    const newRow=document.createElement('tr');
+
+    const cell1=document.createElement('td');
+    cell1.textContent='New Cell1';
+    const cell2=document.createElement('td');
+    cell2.textContent='New Cell2';
+
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+	const tableElement=document.querySelector('table')
+    tableElement.insertBefore(newRow, tableElement.firstChild);
+}
 }
